2265c591ac07e90952cf1e655d1bfa3ef82bd980
CarlisleChan <duziyixiangsi@gmail.com>
2017-09-01 11:21:32 +0800

    update theme

--- app/src/main/java/com/carlisle/seed/SeedApplication.java
+++ app/src/main/java/com/carlisle/seed/SeedApplication.java
@@ -1,8 +1,14 @@
 package com.carlisle.seed;
 
+import android.content.Context;
+
+import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.Utils;
 import com.carlisle.framework.swipeback.common.SwipeBackApplication;
 import com.carlisle.seed.provider.db.RealmUtil;
+import com.google.gson.Gson;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
  * Creator      : carlisle
@@ -11,10 +17,49 @@ import com.carlisle.seed.provider.db.RealmUtil;
  */
 
 public class SeedApplication extends SwipeBackApplication {
+    public static final String APP_DIR_NAME = "Spring";
+    private static final String TAG = "SpringApplication";
+
+    private static Context instance;
+    private static Gson gson;
+
+    public static Context from() {
+        return instance;
+    }
+
+    public static Gson getGson() {
+        if (gson == null)
+            gson = new Gson();
+        return gson;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        Utils.init(this);
+        instance = getApplicationContext();
+
+        long firstTime = System.currentTimeMillis();
+
+        initRealm();
+        initFontConfig();
+        initUtils();
+
+        LogUtils.d(TAG, "time:" + (System.currentTimeMillis() - firstTime));
+    }
+
+    private void initRealm() {
         RealmUtil.init(this);
+    }
+
+    private void initUtils() {
+        Utils.init(this);
+    }
+
+    private void initFontConfig() {
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/BrandonTextRegular.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
     }
 }
